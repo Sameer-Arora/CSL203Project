5c79952b1f3ff566300ac3dcb9e0c4e15639122c
Sameer-Arora <33423842+Sameer-Arora@users.noreply.github.com>
2018-04-09 13:25:49 +0530

    login Files

---
+++ TnpCell/login.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Login Page:Internal Tnp Cell</title>
+        <link href="css/login.css" rel="stylesheet" type="text/css"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js" type="text/javascript"></script>
+
+		<!-- jQuery UI Widget and Effects Core (custom download)
+			 You can make your own at: http://jqueryui.com/download -->
+		<script src="./js/jquery-ui-1.10.3.custom.min.js" type="text/javascript"></script>
+			
+   <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+   <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" rel="stylesheet">
+   
+    </head>
+    <body>
+        <?php
+        require 'login.html';
+        ?>
+    </body>
+</html>
